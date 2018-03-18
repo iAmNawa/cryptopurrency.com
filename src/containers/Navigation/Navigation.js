d4e094afebd88ba4aa5c29bcb49cddbda774f76c
Paul Borawski <paulborawski14@gmail.com>
2018-03-17 19:27:24 -0700

    added crypto logos

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import './Navigation.css';
+import btc from '../../assets/images/btc1.png';
+import ltc from '../../assets/images/ltc.png';
+import eth from '../../assets/images/eth.png';
 
 class Navigation extends Component {
  render () {
@@ -9,9 +12,9 @@ class Navigation extends Component {
        <nav>
           <ul>
                  <li><a href="/">Home</a></li>
-                 <li><a href="/">BTC</a></li>
-                 <li><a href="/">LTC</a></li>
-                 <li><a href="/">ETH</a></li>
+                 <li><a href="/"><img src={btc} /></a></li>
+                 <li><a href="/"><img src={ltc} /></a></li>
+                 <li><a href="/"><img src={eth} /></a></li>
           </ul>
        </nav>
      </header>
