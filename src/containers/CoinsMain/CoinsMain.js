c82f491144371ded3415c7659d2933d8f11510e6
Paul Borawski <paulborawski14@gmail.com>
2018-03-17 20:18:10 -0700

    btc price updating every 2 seconds

--- src/containers/CoinsMain/CoinsMain.js
+++ src/containers/CoinsMain/CoinsMain.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 
 import './CoinsMain.css'
 
+const ROOT_URL = 'http://cryptopurrency.com:45789/results';
+
 class CoinsMain extends Component {
+  state = {
+    btcprice: 0
+  }
+
+  letUsGetResults = () => {
+    var request = new XMLHttpRequest();
+    //request.addEventListener("load", letUsGetResults);
+    request.open('GET', ROOT_URL);
+    request.responseType = 'json';
+
+    request.onload = function() {
+      this.setState({btcprice: request.response.price})
+      console.log(request.response.price)
+    }.bind(this)
+    request.send();
+  }
+
+  componentDidMount = () => {
+     this.interval = setInterval(this.letUsGetResults, 2000);
+   }
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval);
+  }
+
  render () {
    return (
      <div className="coinsList">
         <div className="coinsListElements">Coins List:</div>
         <hr className="hrWidth" />
-        <div className="coins">Btc:</div>
-        <div className="coins">Ltc:</div>
-        <div className="coins">Eth:</div>
+        <div className="coins">BTC: {this.state.btcprice}</div>
+        <div className="coins">LTC:</div>
+        <div className="coins">ETH:</div>
      </div>
    )
  }
