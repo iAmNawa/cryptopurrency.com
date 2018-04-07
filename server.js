ae5d4c47adce04b23452db2272e1a5feec9e207c
Paul Borawski <paulborawski14@gmail.com>
2018-04-07 11:19:31 -0700

    express serving the build folder

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5055;
+
+app.use(express.static('build'))
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
