f72d348d8b86e627cab5a09ca9b01d92eafe4798
Paul Borawski <paulborawski14@gmail.com>
2018-04-07 11:07:53 -0700

    added proxy in package.json to request data

--- src/App.js
+++ src/App.js
@@ -6,11 +6,31 @@ import CoinsMain from './containers/CoinsMain/CoinsMain';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navigation />
+          <div>{this.state.response}</div>
         </div>
       </BrowserRouter>
     );
